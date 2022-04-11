b581d0e6e48d573d1d2133cb92cb7301285436c5
raya <raya@rhp-valuation.com>
2022-04-11 14:26:16 +0700

    vercel

--- components/wp/Table.vue
+++ components/wp/Table.vue
@@ -29,26 +29,44 @@
             <tr>
               <td>Koordinat</td>
               <td>3.61386, 98.71646</td>
+              <template v-for="n in 3">
+                <td :key="n" colspan="3">3.61386, 98.71646</td>
+              </template>
             </tr>
             <tr>
               <td>Contact Person</td>
-              <td>Budi / 08123 45678.</td>
+              <td>Budi / 08123 45678</td>
+              <template v-for="n in 3">
+                <td :key="n" colspan="3">Budi / 08123 45678</td>
+              </template>
             </tr>
             <tr>
               <td>Jenis Data</td>
-              <td>-</td>
+              <td></td>
+              <template v-for="n in 3">
+                <td :key="n" colspan="3">Penawaran</td>
+              </template>
             </tr>
             <tr>
               <td>Tanggal Transaksi / Penawaran</td>
-              <td>-</td>
+              <td></td>
+              <template v-for="n in 3">
+                <td :key="n" colspan="3">10 Januari 2022</td>
+              </template>
             </tr>
             <tr>
               <td>Jenis Properti</td>
-              <td>Rumah</td>
+              <td>Rumah Tinggal</td>
+              <template v-for="n in 3">
+                <td :key="n" colspan="3">Rumah Tinggal</td>
+              </template>
             </tr>
             <tr>
               <td>Foto</td>
-              <td>Foto</td>
+              <td></td>
+              <template v-for="n in 3">
+                <td :key="n" colspan="3"></td>
+              </template>
             </tr>
             <tr>
               <td>Luas Tanah</td>
@@ -56,6 +74,12 @@
                 100 m
                 <sup>2</sup>
               </td>
+              <template v-for="n in 3">
+                <td :key="n" colspan="3">
+                  100 m
+                  <sup>2</sup>
+                </td>
+              </template>
             </tr>
             <tr>
               <td>Luas Bangunan</td>
@@ -63,6 +87,12 @@
                 180 m
                 <sup>2</sup>
               </td>
+              <template v-for="n in 3">
+                <td :key="n" colspan="3">
+                  180 m
+                  <sup>2</sup>
+                </td>
+              </template>
             </tr>
             <tr>
               <td class="t-separator">Unit Perbandingan</td>
@@ -71,22 +101,37 @@
             <tr>
               <td>Harga Penawaran</td>
               <td></td>
+              <template v-for="n in 3">
+                <td :key="n" colspan="3">1.500.000.000</td>
+              </template>
             </tr>
             <tr>
               <td>Harga Penawaran Tertinggi</td>
               <td></td>
+              <template v-for="n in 3">
+                <td :key="n" colspan="3">0</td>
+              </template>
             </tr>
             <tr>
               <td>Diskon</td>
               <td></td>
+              <template v-for="n in 3">
+                <td :key="n" colspan="3">5 %</td>
+              </template>
             </tr>
             <tr>
               <td>Kemungkinan Transaksi</td>
               <td></td>
+              <template v-for="n in 3">
+                <td :key="n" colspan="3">1.425.000.000</td>
+              </template>
             </tr>
             <tr>
               <td>Indikasi Nilai Bangunan</td>
               <td></td>
+              <template v-for="n in 3">
+                <td :key="n" colspan="3">281.200.000</td>
+              </template>
             </tr>
             <tr>
               <td>
@@ -94,10 +139,16 @@
                 <sup>2</sup>
               </td>
               <td></td>
+              <template v-for="n in 3">
+                <td :key="n" colspan="3">3.515.000</td>
+              </template>
             </tr>
             <tr>
               <td>Indikasi Harga Transaksi</td>
               <td></td>
+              <template v-for="n in 3">
+                <td :key="n" colspan="3">1.143.800.000</td>
+              </template>
             </tr>
             <tr>
               <td>
@@ -105,6 +156,9 @@
                 <sup>2</sup>
               </td>
               <td></td>
+              <template v-for="n in 3">
+                <td :key="n" colspan="3">1.634.000</td>
+              </template>
             </tr>
             <tr>
               <td class="t-separator">Penyesuaian Transaksi</td>
@@ -210,6 +264,364 @@
                 <td :key="n">0 %</td>
                 <td :key="n" class="text-right">123.000</td>
               </template>
+            </tr>
+            <tr>
+              <td>Kelas Jalan</td>
+              <td>Jalan Arteri</td>
+              <template v-for="n in 3">
+                <td :key="n">Jalan Kolektor</td>
+                <td :key="n">0 %</td>
+                <td :key="n" class="text-right">123.000</td>
+              </template>
+            </tr>
+            <tr>
+              <td>Lebar Jalan</td>
+              <td>10 m</td>
+              <template v-for="n in 3">
+                <td :key="n">10 m</td>
+                <td :key="n">0 %</td>
+                <td :key="n" class="text-right">123.000</td>
+              </template>
+            </tr>
+            <tr>
+              <td>Jumlah Jalur</td>
+              <td>1</td>
+              <template v-for="n in 3">
+                <td :key="n">1</td>
+                <td :key="n">0 %</td>
+                <td :key="n" class="text-right">123.000</td>
+              </template>
+            </tr>
+            <tr>
+              <td>Jumlah Lajur</td>
+              <td>2</td>
+              <template v-for="n in 3">
+                <td :key="n">2</td>
+                <td :key="n">0 %</td>
+                <td :key="n" class="text-right">123.000</td>
+              </template>
+            </tr>
+            <tr>
+              <td>Perkerasan Jalan</td>
+              <td>Aspal</td>
+              <template v-for="n in 3">
+                <td :key="n">Aspal</td>
+                <td :key="n">0 %</td>
+                <td :key="n" class="text-right">123.000</td>
+              </template>
+            </tr>
+            <tr>
+              <td>Aksesibilitas</td>
+              <td>Baik</td>
+              <template v-for="n in 3">
+                <td :key="n">Baik</td>
+                <td :key="n">0 %</td>
+                <td :key="n" class="text-right">123.000</td>
+              </template>
+            </tr>
+            <tr>
+              <td>Kondisi Lingkungan Sekitar</td>
+              <td>Campuran</td>
+              <template v-for="n in 3">
+                <td :key="n">Campuran</td>
+                <td :key="n">0 %</td>
+                <td :key="n" class="text-right">123.000</td>
+              </template>
+            </tr>
+            <tr>
+              <td class="t-separator">Karateristik Fisik</td>
+              <td class="t-separator" :colspan=" 1 + (3*3)"></td>
+            </tr>
+            <tr>
+              <td>Luas Tanah</td>
+              <td>
+                100 m
+                <sup>2</sup>
+              </td>
+              <template v-for="n in 3">
+                <td :key="n">
+                  100 m
+                  <sup>2</sup>
+                </td>
+                <td :key="n">0 %</td>
+                <td :key="n" class="text-right">123.000</td>
+              </template>
+            </tr>
+            <tr>
+              <td>Lebar Muka</td>
+              <td>19</td>
+              <template v-for="n in 3">
+                <td :key="n">19</td>
+                <td :key="n">0 %</td>
+                <td :key="n" class="text-right">123.000</td>
+              </template>
+            </tr>
+            <tr>
+              <td>Bentuk Tapak</td>
+              <td>Persegi Panjang</td>
+              <template v-for="n in 3">
+                <td :key="n">Persegi Panjang</td>
+                <td :key="n">0 %</td>
+                <td :key="n" class="text-right">123.000</td>
+              </template>
+            </tr>
+            <tr>
+              <td>Elevasi</td>
+              <td>+ 1</td>
+              <template v-for="n in 3">
+                <td :key="n">+ 1</td>
+                <td :key="n">0 %</td>
+                <td :key="n" class="text-right">123.000</td>
+              </template>
+            </tr>
+            <tr>
+              <td>Posisi</td>
+              <td>Tengah</td>
+              <template v-for="n in 3">
+                <td :key="n">Tengah</td>
+                <td :key="n">0 %</td>
+                <td :key="n" class="text-right">123.000</td>
+              </template>
+            </tr>
+            <tr>
+              <td>Kondisi Tapak</td>
+              <td>Darat</td>
+              <template v-for="n in 3">
+                <td :key="n">Darat</td>
+                <td :key="n">0 %</td>
+                <td :key="n" class="text-right">123.000</td>
+              </template>
+            </tr>
+            <tr>
+              <td>Topografi</td>
+              <td>Berbukit</td>
+              <template v-for="n in 3">
+                <td :key="n">Berbukit</td>
+                <td :key="n">0 %</td>
+                <td :key="n" class="text-right">123.000</td>
+              </template>
+            </tr>
+            <tr>
+              <td class="t-separator">Karakteristik Ekonomi</td>
+              <td class="t-separator" :colspan=" 1 + (3*3)"></td>
+            </tr>
+            <tr>
+              <td>Kualitas Pendatapan</td>
+              <td>Baik</td>
+              <template v-for="n in 3">
+                <td :key="n">Baik</td>
+                <td :key="n">0 %</td>
+                <td :key="n" class="text-right">123.000</td>
+              </template>
+            </tr>
+            <tr>
+              <td>Biaya Operasional</td>
+              <td>Tidak Ada</td>
+              <template v-for="n in 3">
+                <td :key="n">Tidak Ada</td>
+                <td :key="n">0 %</td>
+                <td :key="n" class="text-right">123.000</td>
+              </template>
+            </tr>
+            <tr>
+              <td>Ketentuan Sewa</td>
+              <td>Tidak Ada</td>
+              <template v-for="n in 3">
+                <td :key="n">Tidak Ada</td>
+                <td :key="n">0 %</td>
+                <td :key="n" class="text-right">123.000</td>
+              </template>
+            </tr>
+            <tr>
+              <td>Manajemen</td>
+              <td>Tidak Ada</td>
+              <template v-for="n in 3">
+                <td :key="n">Tidak Ada</td>
+                <td :key="n">0 %</td>
+                <td :key="n" class="text-right">123.000</td>
+              </template>
+            </tr>
+            <tr>
+              <td>Bauran Penyewa</td>
+              <td>Baik</td>
+              <template v-for="n in 3">
+                <td :key="n">Baik</td>
+                <td :key="n">0 %</td>
+                <td :key="n" class="text-right">123.000</td>
+              </template>
+            </tr>
+            <tr>
+              <td class="t-separator">Penggunaan</td>
+              <td class="t-separator" :colspan=" 1 + (3*3)"></td>
+            </tr>
+            <tr>
+              <td>Peruntukan</td>
+              <td>Zona Perumahan KDB Rendah</td>
+              <template v-for="n in 3">
+                <td :key="n">Zona Perumahan KDB Rendah</td>
+                <td :key="n">0 %</td>
+                <td :key="n" class="text-right">123.000</td>
+              </template>
+            </tr>
+            <tr>
+              <td>KDB</td>
+              <td>0</td>
+              <template v-for="n in 3">
+                <td :key="n">0</td>
+                <td :key="n">0 %</td>
+                <td :key="n" class="text-right">123.000</td>
+              </template>
+            </tr>
+            <tr>
+              <td>KLB</td>
+              <td>0</td>
+              <template v-for="n in 3">
+                <td :key="n">0</td>
+                <td :key="n">0 %</td>
+                <td :key="n" class="text-right">123.000</td>
+              </template>
+            </tr>
+            <tr>
+              <td>Ketinggian Bangunan</td>
+              <td>0</td>
+              <template v-for="n in 3">
+                <td :key="n">0</td>
+                <td :key="n">0 %</td>
+                <td :key="n" class="text-right">123.000</td>
+              </template>
+            </tr>
+            <tr>
+              <td>GSB</td>
+              <td>0</td>
+              <template v-for="n in 3">
+                <td :key="n">0</td>
+                <td :key="n">0 %</td>
+                <td :key="n" class="text-right">123.000</td>
+              </template>
+            </tr>
+            <tr>
+              <td class="t-separator">Lainnya</td>
+              <td class="t-separator" :colspan=" 1 + (3*3)"></td>
+            </tr>
+            <tr>
+              <td>...</td>
+              <td>...</td>
+              <template v-for="n in 3">
+                <td :key="n">...</td>
+                <td :key="n">0 %</td>
+                <td :key="n" class="text-right">123.000</td>
+              </template>
+            </tr>
+            <tr>
+              <td>...</td>
+              <td>...</td>
+              <template v-for="n in 3">
+                <td :key="n">...</td>
+                <td :key="n">0 %</td>
+                <td :key="n" class="text-right">123.000</td>
+              </template>
+            </tr>
+            <tr>
+              <td>...</td>
+              <td>...</td>
+              <template v-for="n in 3">
+                <td :key="n">...</td>
+                <td :key="n">0 %</td>
+                <td :key="n" class="text-right">123.000</td>
+              </template>
+            </tr>
+            <tr>
+              <td class="t-separator">Kesimpulan</td>
+              <td class="t-separator" :colspan=" 1 + (3*3)"></td>
+            </tr>
+            <tr>
+              <td>Net Adjustment</td>
+              <td></td>
+              <template v-for="n in 3">
+                <td :key="n"></td>
+                <td :key="n">0 %</td>
+                <td :key="n" class="text-right">123.000</td>
+              </template>
+            </tr>
+            <tr>
+              <td>Harga Setelah Adjustment</td>
+              <td></td>
+              <template v-for="n in 3">
+                <td :key="n"></td>
+                <td :key="n"></td>
+                <td :key="n" class="font-semibold text-right">123.000</td>
+              </template>
+            </tr>
+            <tr>
+              <td class="t-separator">Rekonsiliasi</td>
+              <td class="t-separator" :colspan=" 1 + (3*3)"></td>
+            </tr>
+            <tr>
+              <td>Gross Adjustment</td>
+              <td>79,84 %</td>
+              <template v-for="n in 3">
+                <td :key="n"></td>
+                <td :key="n"></td>
+                <td :key="n" class="font-semibold text-right">123.000</td>
+              </template>
+            </tr>
+            <tr>
+              <td>Bobot Absolute</td>
+              <td>100,00 %</td>
+              <template v-for="n in 3">
+                <td :key="n"></td>
+                <td :key="n"></td>
+                <td :key="n" class="font-semibold text-right">123.000</td>
+              </template>
+            </tr>
+            <tr>
+              <td>Inverse</td>
+              <td>200,00 %</td>
+              <template v-for="n in 3">
+                <td :key="n"></td>
+                <td :key="n"></td>
+                <td :key="n" class="font-semibold text-right">123.000</td>
+              </template>
+            </tr>
+            <tr>
+              <td>Bobot Inverse</td>
+              <td>100,00 %</td>
+              <template v-for="n in 3">
+                <td :key="n"></td>
+                <td :key="n"></td>
+                <td :key="n" class="font-semibold text-right">123.000</td>
+              </template>
+            </tr>
+            <tr>
+              <td class="t-separator">Kisaran Nilai</td>
+              <td class="t-separator" :colspan=" 1 + (3*3)"></td>
+            </tr>
+            <tr>
+              <td>Min</td>
+              <td class="text-right">1.000.000</td>
+            </tr>
+            <tr>
+              <td>Max</td>
+              <td class="text-right">2.000.000</td>
+            </tr>
+            <tr>
+              <td>Deviasi</td>
+              <td class="text-right">10 %</td>
+            </tr>
+            <tr>
+              <td class="t-nilai-permeter">
+                Nilai ... per m
+                <sup>2</sup>
+              </td>
+              <td class="text-right t-nilai-permeter">1.000.000</td>
+            </tr>
+            <tr>
+              <td>Nilai ...</td>
+              <td class="text-red-500">1.000.000</td>
+            </tr>
+            <tr>
+              <td>Dibulatkan</td>
+              <td class="text-right">1.000.000</td>
             </tr>
           </tbody>
         </table>
